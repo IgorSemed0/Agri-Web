349da0869c24853affb1d2c4118a221ff35db9a9
Igor Semedo <semedoigor4@gmail.com>
2024-04-21 00:23:31 +0100

    Merge from Antonio

--- app/Http/Controllers/DadosController.php
+++ app/Http/Controllers/DadosController.php
@@ -54,45 +54,45 @@ class DadosController extends Controller
         return view('site.index');
     }
 
-   
-
-public function search(Request $request):View{
-   
-    $termoPesquisa = $request->input(' search');
-    $culturas= DB::table('cultura')->get();
- 
-    return view('view.cultura', ['culturas' => $culturas]);
 
 
-}
-public function Diario_index(Diario $diario){
-    
-   $diario=$diario->all();
-   if ($diario->isEmpty()) {
-    return view('site.diario', ['mensagem' => 'Diário vazio.']);
-} 
-   return view('site.diario',compact('diario'));
-}
+    public function search(Request $request):View{
 
-    public function Diario_create() {
-        
-        return view('site.new-diario');
+        $termoPesquisa = $request->input(' search');
+        $culturas= DB::table('cultura')->get();
+
+        return view('view.cultura', ['culturas' => $culturas]);
+
+
     }
-    
-    public function Diario_store(Request $request){
-        dd($request);
-        $title=$request->input('title');
-        $text=$request->input('text');
- Diario::creat([
-'title'=>$title,
-'text'=>$text
- ]);
- return view('site.diario');
+    public function Diario_index(Diario $diario){
+
+    $diario=$diario->all();
+    if ($diario->isEmpty()) {
+        return view('site.diario', ['mensagem' => 'Diário vazio.']);
+    }
+    return view('site.diario',compact('diario'));
     }
 
-    
-    public function Diario_show(){
-        return view();
-    }
+        public function Diario_create() {
 
-}
+            return view('site.new-diario');
+        }
+
+        public function Diario_store(Request $request){
+            dd($request);
+            $title=$request->input('title');
+            $text=$request->input('text');
+    Diario::creat([
+    'title'=>$title,
+    'text'=>$text
+    ]);
+    return view('site.diario');
+        }
+
+
+        public function Diario_show(){
+            return view();
+        }
+
+    }
