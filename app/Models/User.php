da670d06ced43e0581d4df9d8a61c0f4d55224f6
IgorSemed0 <semedoigor4@gmail.com>
2024-07-29 21:46:45 +0100

    Fix: api comunication bug

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,6 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
     use HasApiTokens;
