203a67ab3288f0bf9190281f642deceda1c89e55
Antonio Da Silva <antoniodaassuncaodasilva@gmail.com>
2024-09-02 17:44:58 +0100

    new home

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -4,23 +4,50 @@
 @section('conteudo')
 
 
-<div  class="mx-2" style="background-image: url({{ asset('assets/img/logo/1.jpg') }});
- align-contents:center;
- height:85vh; width:100%;
- background-size:cover;
- background-repeat:no-repeat;
- border-button:20px;
-
-  
- "> <h1 class="text-center pt-5" style="color:#556B2F">Criando Plantações saudável</h1></div>
- <br><br>
+<div class="ml-2 mr-4 parallax" style="
+  background-image: url({{ asset('assets/img/logo/1.jpg') }});
+  height: 90vh; 
+  width: 100%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 13px;
+  display: flex; 
+  align-items: center; 
+  justify-content: center;
+"> <h1 class="text-center pt-" style="color:#556B2F; "><strong>Criando Plantações saudável</strong></h1></div>
+ </div><br>
  <div class="">
-   <h1 class="text-center text-md " style="color:#7B3F00; font-size:27px;">O que é o Agri-Web?</h1>  
-   <p class="text-center" style="font-size:18px; color:black"> Melhora a forma como  plantas, crias e gerencias o teu cultivo com o <strong>Agri-Web </strong> </p>  
+   <h1 class="text-center text-md " style="color:#7B3F00; font-size:28px;"><strong>O que é o Agri-Web?</strong></h1>  
+   <p class="text-center" style="font-size:20px; color:black"> Melhora a forma como  plantas, crias e gerencias o teu cultivo com o
+     <strong style="color:#556B2F">Agri-Web </strong>.</p>  
+    <p class="text-center" style="font-size:19px; color:black">
+    Com o Agri-Web, você tem em mãos as melhores ferramentas para cuidar da sua terra e cultivar com qualidade.
+     <br>Garanta uma produção sustentável, colheitas saudáveis e um futuro mais próspero para o seu negócio agrícola!
+    </p>
+</div><br>
 
-</div>
+    <div class="mx-4 px-3 pt-1" style="   width:33%;height:50vh; border:2px solid #7B3F00;border-radius:12px;">
+      <p class="text-center" style="color:#556B2F; font-size:20px"><strong >Benefício do uso do Agri-Web</strong></p>
+      <ul  style="color:#7B3F00 ; font-size:18px">
+        <li>Planejamento: Otimização</li>
+        <li class="pt-1">Acompanhamento Histórico: Análise</li>
+        <li class="pt-1">Redução de Riscos: Segurança</li>
+        <li class="pt-1">Gestão Personalizada: Customização</li>
+        <li class="pt-1">Melhor Uso dos Recursos: Eficiência</li>
+<li class="pt-1">Diário Agrícola: Registro</li>
+<li class="pt-1">Apoio à Decisão: Suporte</li>
+      </ul>
+    </div>
+    
+    <div  class=" px-3 pt-1" style="   width:33%;height:50vh; border:2px solid #7B3F00;border-radius:12px;margin-left: 45rem;">
+    <p class="text-center" style="color:#556B2F; font-size:20px"><strong > Integração com Dados Climáticos</strong></p>
+  
+    </div>
+<br><br><br>
+  <div><h3 class="text-center text-md " style="color:#7B3F00; font-size:28px;"> <strong>Integração com Dados Climáticos</strong></h3></div>
 
 
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
       
 
 
