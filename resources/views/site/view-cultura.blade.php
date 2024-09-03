897b2de60d56547bdc2ed99495dd8e39306052f5
Antonio Da Silva <antoniodaassuncaodasilva@gmail.com>
2024-09-03 21:07:07 +0100

    new versão 1.3

--- resources/views/site/view-cultura.blade.php
+++ resources/views/site/view-cultura.blade.php
@@ -29,7 +29,7 @@
 <div class="d-flex justify-content-center position-relative">
     <form action="{{route('search')}}" method="get" class="col-6 position-relative ">
         <div class="input-group">
-            <input type="search" name="search" value="{{$termoPesquisa }}" id="search" class="form-control pl-5 position-absolute top-0 start-0 pt-2  " style="height:50px; width: 120%; max-width: 650px ;border-radius: 20px;" placeholder="Faça a sua pesquisa na Agri-Web">
+            <input type="search" name="search" value="{{$termoPesquisa }}" id="search" class="form-control pl-5 position-absolute top-0 start-0 pt-2  " style="color:black;height:50px; width: 120%; max-width: 650px ;border-radius: 20px;" placeholder="Faça a sua pesquisa na Agri-Web">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="23" fill="currentColor" class="bi bi-search ml-4 mt-3 position-relative top-60 end-0 translate-middle-y" type="submit" style="color:black" viewBox="0 0 16 16" >
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                 </svg>
@@ -46,22 +46,24 @@
         
         <div class="card mx-2">
   <div class="card-body">
-    <h5 class="card-title text-center"> {{$cultura->cultureTittle}}</h5>
+    <h5 class="card-title text-center" style="color:black"> {{$cultura->cultureTittle}}</h5>
    
    <br>
-   <p>No Agri-web, estamos aqui para ajudá-lo a determinar o momento ideal para plantar sua cultura ideal "{{$cultura->cultureTittle}}". Para o melhor crescimento da sua cultura , precisamos de alguns dados importantes:</p>
+   <p style="color:black">No Agri-Web, estamos aqui para ajudá-lo a determinar o momento ideal
+     para plantar sua cultura de interesse como: {{$cultura->cultureTittle}}. Para garantir o melhor crescimento,
+     precisamos coletar e analisar alguns dados essenciais sobre o ambiente de cultivo:
+   </p>
 
-
-   <ul>
-    <li>Humidade do solo:{{$cultura->soilHumidity}}</li>
-    <li>Temperatura do solo:{{$cultura->soilTemperature}}</li>
-    <li> Condutividade so solo:{{$cultura->soilConductivity}}</li>
-    <li>Temperatura do ar:{{$cultura->airTemperature}}</li>
-    <li>Humidade do ar:{{$cultura->airHumidity}}</li>
+   <ul style="color:black">
+    <li>Humidade do solo:{{$cultura->soilHumidity}}%</li>
+    <li>Temperatura do solo:{{$cultura->soilTemperature}}°C</li>
+    <li> Condutividade so solo:{{$cultura->soilConductivity}}dS/m</li>
+    <li>Temperatura do ar:{{$cultura->airTemperature}}°C</li>
+    <li>Humidade do ar:{{$cultura->airHumidity}}%</li>
     <li>PH do solo:{{$cultura->soilPH}}</li>
-    <li>Nitrogénio:{{$cultura->nitrogen}}</li>
-    <li>Fósforo:{{$cultura->phosphorus}}</li>
-    <li>Potásio:{{$cultura->potassium}}</li>
+    <li>Nitrogénio:{{$cultura->nitrogen}}mg/kg</li>
+    <li>Fósforo:{{$cultura->phosphorus}}mg/kg</li>
+    <li>Potásio:{{$cultura->potassium}}mg/kg</li>
    </ul>
  
 
